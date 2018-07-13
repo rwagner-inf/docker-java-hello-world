e14311214f56ed67a00d772420c25e40fe815c4a
rwagner_inf <rwagner_inf@hotmail.com>
2018-07-13 10:26:18 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World DESENVOLVIMENTO!");
+        System.out.println("Hello World PRODUCAO!");
         System.out.println(" - java.version: " + System.getProperty("java.version"));
         System.out.println(" - java.vm.version: " + System.getProperty("java.vm.version"));
     }
